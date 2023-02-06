416fccdfd5098440bd3a786d600062ca7e5a3d78
mikito35 <122259412+mikito35@users.noreply.github.com>
2023-02-06 17:34:56 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,35 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :home
+  resources :message
+  resources :illust
+  resources :editor
+  root "home#top"
+
+  get 'message/index' => "message#index"
+  get 'message/show/:id' => "message#show"
+  get 'message/new' => "message#new"
+  post 'message/create' => "message#create"
+  get 'message/answer/:id' => "message#answer"
+  post 'message/update/:id' => "message#update"
+  post 'message/del/:id' => "message#del"
+  
+  get 'illust/index' => "illust#index"
+  get 'illust/show/:id' => "illust#show"
+  get 'illust/showtp/:id' => "illust#showtp"
+  get 'illust/new' => "illust#new"
+  post "illust/create" => "illust#create"
+  get 'illust/edit/:id' => "illust#edit"
+  post 'illust/update/:id' => "illust#update"
+  post 'illust/del/:id' => "illust#del"
+
+
+  get 'editor/login_form' => "editor#login_form"
+  post 'editor/login' => "editor#login"
+  post 'editor/logout' => "editor#logout"
+
+
+  get '/' => "home#top"
 end
+
